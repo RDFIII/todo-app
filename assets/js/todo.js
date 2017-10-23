fa66b3bc08da3832f0e5e6124344043d448b5054
RDF <foulkesr@gmail.com>
2017-10-23 12:04:55 -0400

    previous functionality expanded to html

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -2,3 +2,13 @@
 $("li").click(function(){
   $(this).toggleClass("completed");
 });
+
+$("span").click(function(event){
+  // parent refers to the li, not span
+  $(this).parent().fadeOut(2000, function(){
+    //remove entire li containing span
+    $(this).remove();
+  };
+  // stopPropagation prevents event bubbbling (span => ul and so on)
+  event.stopPropagation();
+})
